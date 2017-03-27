9cdfd36d9869346bda022c1259a5eb5148d25a0d
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2017-03-27 14:16:30 +0300

    Add default unclickable option to combobox

--- src/ui/TeachersExamsFrame.java
+++ src/ui/TeachersExamsFrame.java
@@ -20,13 +20,33 @@ public class TeachersExamsFrame extends JFrame{
     public static JScrollPane scrollPane;
     JComboBox teachersComboBox;
 
+    private static final String NOT_SELECTABLE_OPTION = " - ... - ";
+
     public TeachersExamsFrame() {
         MySQLDAO dao = new MySQLDAO();
 
         teachersComboBox = new JComboBox();
+        teachersComboBox.setModel(new DefaultComboBoxModel() {
+            private static final long serialVersionUID = 1L;
+            boolean selectionAllowed = true;
+
+            @Override
+            public void setSelectedItem(Object anObject) {
+                if (!NOT_SELECTABLE_OPTION.equals(anObject)) {
+                    super.setSelectedItem(anObject);
+                } else if (selectionAllowed) {
+                    // Allow this just once
+                    selectionAllowed = false;
+                    super.setSelectedItem(anObject);
+                }
+            }
+        });
+
+        teachersComboBox.addItem(NOT_SELECTABLE_OPTION);
         for(Teacher x : dao.readTeachers()){
             teachersComboBox.addItem(x.getName());
         }
+        teachersComboBox.setSelectedItem("d");
         Menu.mainFrm.getContentPane().add(teachersComboBox);
 
         table = new JTable(model);
