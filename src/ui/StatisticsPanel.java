c01d639eec64c136e4875a9072ae4022a27311a8
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2017-04-03 03:18:02 +0300

    Statistics now works

--- src/ui/StatisticsPanel.java
+++ src/ui/StatisticsPanel.java
@@ -3,6 +3,7 @@ package ui;
 
 import dao.MySQLDAO;
 import vo.Exam;
+import vo.Result;
 import vo.Teacher;
 
 import javax.swing.*;
@@ -11,6 +12,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
+import java.util.List;
 
 public class StatisticsPanel extends JPanel {
 
@@ -20,16 +22,19 @@ public class StatisticsPanel extends JPanel {
         JPanel statsPanel = new JPanel();
         statsPanel.setLayout(new GridLayout(0, 1));
 
+        List<Result> results = dao.readResultsByCourseExam(course_name);
+
+
         JLabel text1 = new JLabel("Кількість студентів");
-        JLabel a = new JLabel("A: " + dao.studentsWithA(course_name,group_year));
-        JLabel b = new JLabel("B: " + dao.studentsWithB());
-        JLabel c = new JLabel("C: " + dao.studentsWithC());
-        JLabel d = new JLabel("D: " + dao.studentsWithD());
-        JLabel e = new JLabel("E: " + dao.studentsWithE());
-        JLabel f = new JLabel("F: " + dao.studentsWithF());
+        JLabel a = new JLabel("A: " + countA(results));
+        JLabel b = new JLabel("B: " + countB(results));
+        JLabel c = new JLabel("C: " + countC(results));
+        JLabel d = new JLabel("D: " + countD(results));
+        JLabel e = new JLabel("E: " + countE(results));
+        JLabel f = new JLabel("F: " + countF(results));
 
         JLabel notPresent = new JLabel("Не присутні: " + dao.notPresentStudents());
-        JLabel average = new JLabel("Середній бал: " + dao.averageMark(course_name,group_year));
+        JLabel average = new JLabel("Середній бал: " + avg(results));
 
         statsPanel.add(text1);
         statsPanel.add(a);
@@ -41,5 +46,73 @@ public class StatisticsPanel extends JPanel {
         statsPanel.add(notPresent);
         statsPanel.add(average);
         this.add(statsPanel);
+    }
+
+    private double avg(List<Result> results){
+        double sum = 0;
+        double size = results.size();
+        for(Result result : results){
+            sum+=Double.valueOf(result.getMark());
+        }
+        return sum/size;
+    }
+
+    private int countA(List<Result> results){
+        int sum = 0;
+        for(Result result : results){
+            double mark = Double.valueOf(result.getMark());
+            if(mark > 90 )
+            sum++;
+        }
+        return sum;
+    }
+
+    private int countB(List<Result> results){
+        int sum = 0;
+        for(Result result : results){
+            double mark = Double.valueOf(result.getMark());
+            if(mark > 80 && mark < 91 )
+                sum++;
+        }
+        return sum;
+    }
+
+    private int countC(List<Result> results){
+        int sum = 0;
+        for(Result result : results){
+            double mark = Double.valueOf(result.getMark());
+            if(mark > 70 && mark < 81 )
+                sum++;
+        }
+        return sum;
+    }
+
+    private int countD(List<Result> results){
+        int sum = 0;
+        for(Result result : results){
+            double mark = Double.valueOf(result.getMark());
+            if(mark > 65 && mark < 71 )
+                sum++;
+        }
+        return sum;
+    }
+    private int countE(List<Result> results){
+        int sum = 0;
+        for(Result result : results){
+            double mark = Double.valueOf(result.getMark());
+            if(mark > 59 && mark < 66 )
+                sum++;
+        }
+        return sum;
+    }
+
+    private int countF(List<Result> results){
+        int sum = 0;
+        for(Result result : results){
+            double mark = Double.valueOf(result.getMark());
+            if(mark < 60 )
+                sum++;
+        }
+        return sum;
     }
 }
